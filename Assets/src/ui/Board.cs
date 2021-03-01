b715820ba3634e446990a638bcb6dc29f3783ca3
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-01 20:14:02 +0800

    Added methods & attributes to Board based on class diagram

--- Assets/src/ui/Board.cs
+++ Assets/src/ui/Board.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    private static readonly int MAX_LAPS = 14;
+
     public Piece[] pieces;
-    public Tile[] tileObjects;
+    public Tile[] tiles;
+
+    int currentPieceIndex = 0;
+
+    internal void SetSelectedPiece(int index)
+    {
+        currentPieceIndex = index;
+    }
+
+    internal int MovePiece(int diceValue)
+    {
+        // TODO: Replace with actual logic
+        return 0;
+    }
+
+    internal bool hasReachedMaxLaps()
+    {
+        // TODO: Replace with actual logic
+        return false;
+    }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
+        //Gizmos.color = Color.green;
 
-        for (int i = 0; i < tileObjects.Length; i++)
-        {
-            Vector3 currentPos = tileObjects[i].gameObject.transform.position;
+        //for (int i = 0; i < tiles.Length; i++)
+        //{
+        //    Vector3 currentPos = tiles[i].gameObject.transform.position;
 
-            if (i > 0)
-            {
-                Vector3 previousPos = tileObjects[i - 1].gameObject.transform.position;
-                Gizmos.DrawLine(previousPos, currentPos);
-            }
-        }
-    }
-
-    internal void MovePiece()
-    {
-
+        //    if (i > 0)
+        //    {
+        //        Vector3 previousPos = tiles[i - 1].gameObject.transform.position;
+        //        Gizmos.DrawLine(previousPos, currentPos);
+        //    }
+        //}
     }
 }
