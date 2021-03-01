e317703c422803c882258b328f5354bfe30fb4e0
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-01 19:35:56 +0800

    Updated Player class to match class diagram

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour
+﻿public class Player
 {
-    public int playerID;
-    public string Username;
-    public int playerNumber;
-    public Piece piece;
+    public string Name { get; }
+    public int Credit { get; private set; } = 1024;
+
+    public Player(string name)
+    {
+        this.Name = name;
+    }
+
+    public void addCredit(int amount)
+    {
+        Credit += amount;
+    }
 }
