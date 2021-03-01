408b99330c8c9ad6efddc546a3c0a9ce3e50a369
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-01 21:25:54 +0800

    Added methods to GameUi based on class diagram

--- Assets/src/ui/GameUi.cs
+++ Assets/src/ui/GameUi.cs
@@ -13,32 +13,67 @@ public class GameUi : MonoBehaviour
         controller.SetPlayerNames(new string[] { "Apple", "Banana", "Cherry", "Mewtwo" });
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RollDice();
+        }
+    }
+
     void LoadCurrentPlayerDetails()
     {
         // Get player details & display them
         Player currentPlayer = controller.CurrentPlayer;
-        Debug.Log($"{currentPlayer.Name} at position {controller.CurrentPlayerPos} playing now");
 
         // Select player's piece
         board.SetSelectedPiece(controller.CurrentPlayerPos);
     }
 
-    void Update()
+    void RollDice()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // TODO: Move to Roll Dice click event
-            int diceValue = controller.GenerateDiceValue();
-            board.MovePiece(diceValue);
+        int diceValue = controller.GenerateDiceValue();
+        board.MovePiece(diceValue);
 
-            // TODO: Move to End Turn click event
-            bool shouldShowNews = controller.NextTurn();
-            if (shouldShowNews)
-            {
-                // TODO: Display news
-            }
+        // TODO: Remove when end turn button is implemented
+        EndTurn();
 
-            LoadCurrentPlayerDetails();
-        }
+    }
+
+    void OnQuizTileActivated()
+    {
+        // TODO: Launch quiz UI
+    }
+
+    void OnEventTileActivated()
+    {
+        // TODO: Launch event UI
+    }
+
+    void ShowStockMarket()
+    {
+        // TODO: Show view stock market UI
+    }
+
+    void EndTurn()
+    {
+        bool shouldShowNews = controller.NextTurn();
+        if (shouldShowNews) DisplayNews();
+
+        LoadCurrentPlayerDetails();
+    }
+
+    void DisplayNews() {}
+
+    void EndGame()
+    {
+        // TODO: Display confirmation message, then end game
+    }
+
+    void DisplayFinalScores() {}
+
+    void ShowLeaderBoard()
+    {
+        // TODO: Show leaderboard UI
     }
 }
