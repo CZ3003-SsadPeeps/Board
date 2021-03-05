0e62209b8b6a35e5fb1744b25cdd98d17b41b46c
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-05 20:01:46 +0800

    Created test class for stock trader interface

---
+++ Assets/src/test/StockTraderTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+class StockTraderTest : IStockTrader
+{
+    public List<PlayerStock> GetPlayerStocks(string playerName)
+    {
+        return new List<PlayerStock>();
+    }
+
+    public void SellAllStocks(Player[] players)
+    {
+
+    }
+}
