6d91f97907e87409ce1024da6ec5086f8e51eab9
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-08 21:27:18 +0800

    Added name input validation

--- Assets/src/nameinput/NameInputUi.cs
+++ Assets/src/nameinput/NameInputUi.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class NameInputUi : MonoBehaviour
@@ -8,6 +9,51 @@ public class NameInputUi : MonoBehaviour
 
     public void OnStartButtonClick()
     {
+        // Dismiss all error messages
+        foreach(Text errorMessage in errorMessages)
+        {
+            errorMessage.gameObject.SetActive(false);
+        }
 
+        bool areNamesValid = true;
+        string inputFieldText;
+        int i, j;
+        for(i = 0; i < inputFields.Length; i++)
+        {
+            inputFieldText = inputFields[i].text;
+            Debug.Log($"Text at position {i} = {inputFieldText}");
+
+            // Check if name is blank
+            if (!ValidateName(inputFieldText))
+            {
+                areNamesValid = false;
+                errorMessages[i].text = "Name cannot be blank!";
+                errorMessages[i].gameObject.SetActive(true);
+                continue;
+            }
+
+            // Check if someone else has the same name
+            for (j = i - 1; j >= 0; j--)
+            {
+                if (inputFieldText != inputFields[j].text) continue;
+
+                areNamesValid = false;
+                errorMessages[i].text = $"Name cannot be the same as Player {j + 1}'s name";
+                errorMessages[i].gameObject.SetActive(true);
+                break;
+            }
+        }
+
+        Debug.Log($"Are names valid? {areNamesValid}");
+
+        if (!areNamesValid) return;
+
+        // TODO: Go to Game scene
+        Debug.Log("Loading game...");
+    }
+
+    private bool ValidateName(string name)
+    {
+        return !String.IsNullOrWhiteSpace(name);
     }
 }
