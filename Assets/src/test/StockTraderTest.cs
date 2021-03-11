51d1253721289bd8933aae759dcb3a57f8df9819
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-11 21:08:57 +0800

    Added sample data for stock purchases

--- Assets/src/test/StockTraderTest.cs
+++ Assets/src/test/StockTraderTest.cs
@@ -5,7 +5,13 @@ class StockTraderTest : IStockTrader
 {
     public List<PlayerStock> GetPlayerStocks(string playerName)
     {
-        return new List<PlayerStock>();
+        return new List<PlayerStock>() {
+            new PlayerStock("Apple", 12, 44, 63),
+            new PlayerStock("Tesla", 16, 33, 40),
+            new PlayerStock("AMD", 22, 16, 55),
+            new PlayerStock("Raid Shadow Legends", 35, 21, 69),
+            new PlayerStock("Gamestop", 88, 69, 420),
+        };
     }
 
     public void SellAllStocks(Player[] players)
