6e29b9a1d69dffc7ff5ac17f56348ed73f328270
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-12 12:32:14 +0800

    Fixed pop-up not appearing on top of every UI component

--- Assets/src/ui/GameUi.cs
+++ Assets/src/ui/GameUi.cs
@@ -20,6 +20,10 @@ public class GameUi : MonoBehaviour
         // Uncomment when testing Game UI only
         //GameStore.InitPlayers(new string[] { "Abu", "Banana", "Cherry", "Mewtwo" });
         GeneratePlayerCards();
+
+        // Ensures popup is displayed on top of everything else. Must be done after player cards are generated
+        passedGoPopup.transform.SetAsLastSibling();
+
         LoadCurrentPlayerDetails();
     }
 
